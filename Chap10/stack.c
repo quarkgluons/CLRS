c6088d9ed3a1e8bb18384f7668c4b66868773a01
quarkgluons <quarkgluons@gmail.com>
2015-03-02 20:15:59 +0530

    added create_stack, pop and stack_empty function

---
+++ Chap10/stack.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_SIZE 10
+
+typedef struct  {
+  int arr[STACK_SIZE];
+  int tos;
+} stack;
+
+
+stack * create_stack ()
+{
+  stack * s =  malloc(sizeof(stack));
+  s->tos = -1;
+  return s;
+}
+
+int stack_empty(stack * s)
+{
+  if (s->tos == -1)
+    return 1;
+  return 0;
+}
+
+int pop(stack *s)
+{
+  if (stack_empty(s)) {
+    printf("Stack UnderFlow\n");
+    exit(-1);
+  }
+  s->tos--;
+  return s->arr[s->tos+1];
+}
+
+int main(void)
+{
+  stack * S = create_stack();
+  printf("%d\n", S->tos);
+  return 0;
+}
